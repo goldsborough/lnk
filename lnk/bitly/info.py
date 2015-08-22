25cf2a7c7ab52e3aefa6dc839dd73056ec37b7d0
petergoldsborough <petergoldsborough@hotmail.com>
2015-08-22 18:59:10 +0200

    Implemented info command for bitly

--- lnk/bitly/info.py
+++ lnk/bitly/info.py
@@ -1,61 +1,77 @@
 #!/usr/bin/env python
 #! -*- coding: utf-8 -*-
 
+import click
+import time
+
 from command import Command
 
-def handle(*args):
-	Info(args)
+def echo(*args):
+	click.echo(Info().fetch(*args))
 
 class Info(Command):
-	def __init__(self, args):
+
+	def __init__(self):
 		super(Info, self).__init__("bitly", "info")
-		self.parse(args)
+		self.sets = self.config['sets']
+		self.reverse = {value:key for key,value in self.sets.items()}
 
-	def parse(self, args):
+	def fetch(self, only, hide, urls):
+		sets = self.sets
+		if only:
+			sets = {k:v for k,v in sets.items() if k in only}
+		for key in hide:
+			del sets[key]
 
-		sets = args.only if args.only else self.config["sets"].keys()
+		maximum = 0
+		results = []
+		for url in urls:
+			data = self.get(url, sets)
+			result, width = self.lineify(url, data, sets)
+			if width > maximum:
+				maximum = width
+			results.append(result)
 
-		if args.hide:
-			for i in args.hide:
-				sets.remove(i)
+		return '\n'.join(self.boxify(results, width))
 
-		sets = [self.config["sets"][key] for key in sets]
+	def lineify(self, url, data, sets): 
+		line = 'URL: {0}'.format(url)
+		width = len(line)
+		lines = [line]
+		for key, value in data.items():
+			line = self.format(key, value)
+			if len(line) > width:
+				width = len(line)
+			lines.append(line)
+		return lines, width
 
-		info = []
+	def format(self, key, value):
+		key = self.reverse[key]
+		if key == 'created':
+			value = time.ctime(value)
+		elif key == "user" and value is None:
+			value = "Not public"
 
-		for url in args.urls:
+		return '{0}: {1}'.format(key.title(), value)
 
-			result = self.retrieve(url, sets)
+	def boxify(self, results, width):
+		border = width + 2
+		lines = ['┌{}┐'.format('─' * border)]
+		for n, result in enumerate(results):
+			for line in result:
+				line = '│ {0} │'.format(line.ljust(width))
+				lines.append(line)
+			if n + 1 < len(results):
+				lines.append('├{}┤'.format('─' * border))
+		return lines + ['└{}┘'.format('─' * border)]
 
-			mapped = {}
-
-			# Reverse map
-			for i, j in result.items():
-				for k, l in self.config["sets"].items():
-					if i == l:
-						if k == "created":
-							# UNIX timestamp to string
-							j = time.ctime(j)
-						elif k == "user" and not j:
-							j = "Not public"
-						mapped[k] = j  
-
-			mapped["url"] = url
-
-			info.append(mapped)
-
-		return info
-
-	def retrieve(self, url, sets):
-
+	def get(self, url, sets):
 		self.parameters["shortUrl"] = url
 
-		response = self.request(self.config["endpoints"]["info"])
-
-		self.check(response,
-				   "retrieve information for " + url,
+		response = self.request(self.endpoints["info"])
+		self.verify(response,
+				   "retrieve information for {0}".format(url),
 				   "info")
 
 		response = response["data"]["info"][0]
-
-		return {i : response[i] for i in response if i in sets}
+		return {k:v for k,v in response.items() if k in sets.values()}
