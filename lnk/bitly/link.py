cc5518b0bfbb6945df3b9b23cf1035a1a670fc86
petergoldsborough <petergoldsborough@hotmail.com>
2015-09-17 02:13:57 +0200

    Wrote documentation for bitly.link

--- lnk/bitly/link.py
+++ lnk/bitly/link.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 #! -*- coding: utf-8 -*-
 
+"""Link shortening and expansion for the bit.ly client."""
+
 import click
 import ecstasy
 import pyperclip
@@ -14,6 +16,19 @@ def echo(*args):
 	click.echo(Link().fetch(*args))
 
 class Link(Command):
+	"""
+	Class to shorten or expand a url using bit.ly.
+
+	This class can shorten a long url to a shortened bit.ly url,
+	or expand a shortened bit.ly url to its original long url.
+
+	Attributes:
+		raw (bool): Whether to prettify the output or
+					return it raw, for internal use.
+		already_copied (bool): Flag set the first time a url is copied
+							   to the clipboard, such that only the first
+							   is copied.
+	"""
 
 	def __init__(self, raw=False):
 		super(Link, self).__init__('link')
@@ -21,6 +36,25 @@ class Link(Command):
 		self.raw = raw
 
 	def fetch(self, copy, quiet, expand, shorten, pretty):
+		"""
+		Fetches a short or expanded link.
+
+		Arguments:
+			copy (bool): Whether or not to copy the first shortened or
+						 expanded link to the clipboard.
+			quiet (bool): Whether or not to swallow warnings about urls
+						  having to be modified because they do not have
+						  a protocol specified (i.e. no 'http://').
+			expand (tuple): A tuple of short urls to expand.
+			shorten (tuple): A tuple of long urls to shorten.
+			pretty (bool): Whether or not to make the output pretty (in a box).
+
+		Returns:
+			A raw list of lines for other internal use if self.raw is True,
+			else a pretty list in a box if pretty is True, else the same
+			list as in the first case, but joined to a string for output.
+		"""
+		self.already_copied = False
 		result = self.shorten_urls(copy, quiet, shorten)
 		result += self.expand_urls(copy, expand)
 
@@ -29,6 +63,19 @@ class Link(Command):
 		return beauty.boxify([result]) if pretty else '\n'.join(result)
 
 	def shorten_urls(self, copy, quiet, urls):
+		"""
+		Shortens a sequence of long urls.
+
+		Arguments:
+			copy (bool): Whether or not to copy the first url to the clipboard.
+			quiet (bool): Whether or not to swallow warnings about urls
+						  having to be modified because they do not have
+						  a protocol specified (i.e. no 'http://').
+			urls (tuple): The tuple of long urls to shorten.
+
+		Returns:
+			A list of lines for output.
+		"""
 		lines = []
 		threads = []
 		for url in urls:
@@ -42,6 +89,16 @@ class Link(Command):
 		return lines
 
 	def expand_urls(self, copy, urls):
+		"""
+		Expands a sequence of short urls.
+
+		Arguments:
+			copy (bool): Whether or not to copy the first url to the clipboard.
+			urls (tuple): The tuple of short urls to expand.
+
+		Returns:
+			A list of lines for output.
+		"""
 		lines = []
 		threads = []
 		for url in urls:
@@ -52,6 +109,18 @@ class Link(Command):
 		return lines
 
 	def shorten(self, lines, copy):
+		"""
+		Requests a shortend link and appends it to a given list.
+
+		A shortened link is retrieved via get_short(), then (possibly)
+		copied to the clipboard via copy(), then appended to the given
+		list in a thread-safe (locked) way.
+
+		Arguments:
+			lines (list): The list of lines to append the url to.
+			copy (bool): Whether or not to copy the link to the clipboard
+						 (if no other link has already been copied).
+		"""
 		url = self.queue.get()
 		short = self.get_short(url)
 		formatted = self.copy(copy, short)
@@ -59,6 +128,18 @@ class Link(Command):
 			lines.append('{0} => {1}'.format(url, formatted))
 
 	def expand(self, lines, copy):
+		"""
+		Requests an expanded link and appends it to a given list.
+
+		An expanded link is retrieved via get_long(), then (possibly)
+		copied to the clipboard via copy(), then appended to the given
+		list in a thread-safe (locked) way.
+
+		Arguments:
+			lines (list): The list of lines to append the url to.
+			copy (bool): Whether or not to copy the link to the clipboard
+						 (if no other link has already been copied).
+		"""
 		url = self.queue.get()
 		expanded = self.get_long(url)
 		formatted = self.copy(copy, expanded)
@@ -66,12 +147,30 @@ class Link(Command):
 			lines.append('{0} => {1}'.format(url, formatted))
 
 	def get_short(self, url):
+		"""
+		Requests and returns a short url for a long one.
+
+		Arguments:
+			url (str): The long url to shorten.
+
+		Returns:
+			The shortened link.
+		"""
 		response = self.get(self.endpoints['shorten'], dict(longUrl=url))
 		response = self.verify(response, "shorten url '{0}'".format(url))
 		
 		return response['url']
 
 	def get_long(self, url):
+		"""
+		Requests and returns an expanded url for a short one.
+
+		Arguments:
+			url (str): The short bit.ly link (bitlink) to expand.
+
+		Returns:
+			The expanded link.
+		"""
 		response = self.get(self.endpoints['expand'], dict(shortUrl=url))
 		response = self.verify(response,
 							   "expand url '{0}'".format(url),
@@ -80,8 +179,28 @@ class Link(Command):
 		return response['long_url']
 
 	def copy(self, copy, url):
+		"""
+		Copies a url to the clipboard if possible.
+
+		"If possible" means the url is only copied if no other url
+		has so-far been copied to the clipboard during the same
+		fetch() call, and also only if the 'copy' is set. If the url
+		is copied, it is formatted using ecstasy to make it bold.
+
+		Arguments:
+			copy (bool): Whether or not to allow copying
+						 to the clipboard at all.
+			url (str): The url to (possibly) copy.
+
+		Returns:
+			The original url if the 'copy' flag is unset or another link
+			has already been copied to the clipboard during the same call
+			to fetch(), else the url, formatted with ecstasy to appear bold
+			in the terminal.
+		"""
 		if copy and not self.already_copied:
 			self.already_copied = True
 			pyperclip.copy(url)
 			url = ecstasy.beautify('<{0}>'.format(url), ecstasy.Style.Bold)
+
 		return url
