89a23ff0e6771e490dad769e349c609b946c1fa2
petergoldsborough <petergoldsborough@hotmail.com>
2015-09-17 21:01:17 +0200

    Documented googl.key

--- lnk/googl/key.py
+++ lnk/googl/key.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+"""Handles the oauth2 authorization procedure for the goo.gl API."""
+
 import click
 import oauth2client.file
 import oauth2client.tools
@@ -20,10 +22,36 @@ SCOPE = 'https://www.googleapis.com/auth/urlshortener'
 REDIRECT_URI = 'urn:ietf:wg:oauth:2.0:oob'
 
 def echo(*args):
+	"""
+	Executes a key command and echoes its output.
+
+	Arguments:
+		args (variadic): The arguments to pass to a
+						 Key instance's fetch() method.
+	"""
 	Key().fetch(*args)
 
 class Key(Command):
+	"""
+	Handles oauth2 authorization for the goo.gl API.
 
+	The authorization procedure involves exchaning a temporary authorization
+	code for an oauth2 token. When executing this command, the user will be
+	redirected to a web-page where he or she can authorize lnk to access his
+	or her private goo.gl information. Once done the user gets an
+	authorization-code which he or she must then paste into the command-line,
+	where it is fetched by lnk and stored for authorization during HTTP requests.
+
+	Attributes:
+		raw (bool): Whether or not to return the raw output for internal use
+					or the prettified output for outside representation.
+		flow (oauth2client.client.OAuth2WebServerFlow): An object from Google's
+										API client that is used for the entire
+										authorization-code-to-oauth2-token
+										exchange procedure.
+		credentials_path (str): The path to the credentials file in the config
+								folder.
+	"""
 	def __init__(self, raw=False):
 		super(Key, self).__init__('key')
 		self.raw = raw
@@ -31,7 +59,7 @@ class Key(Command):
 										client_secret=CLIENT_SECRET,
 										scope=SCOPE,
 										redirect_uri=REDIRECT_URI)
-		self.destination = os.path.join(config.CONFIG_PATH, 'credentials')
+		self.credentials_path = os.path.join(config.CONFIG_PATH, 'credentials')
 
 
 	def fetch(self, _):
@@ -52,5 +80,5 @@ class Key(Command):
 		# the access_token, which expires after a certain time (~ 1 hour).
 		credentials = self.flow.step2_exchange(code)
 
-		storage = oauth2client.file.Storage(self.destination)
+		storage = oauth2client.file.Storage(self.credentials_path)
 		storage.put(credentials)
